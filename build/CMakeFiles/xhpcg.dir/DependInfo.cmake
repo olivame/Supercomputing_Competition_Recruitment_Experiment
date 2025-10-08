902b8cd831e3a04a8b0e601ec3897700ce00ebd7
olivame <olivame@localhost.localdomain>
2025-10-08 23:46:28 +0800

    Initial commit of my version

---
+++ build/CMakeFiles/xhpcg.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/opt/hpcg/src/CG.cpp" "CMakeFiles/xhpcg.dir/src/CG.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/CG.cpp.o.d"
+  "/opt/hpcg/src/CG_ref.cpp" "CMakeFiles/xhpcg.dir/src/CG_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/CG_ref.cpp.o.d"
+  "/opt/hpcg/src/CheckAspectRatio.cpp" "CMakeFiles/xhpcg.dir/src/CheckAspectRatio.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/CheckAspectRatio.cpp.o.d"
+  "/opt/hpcg/src/CheckProblem.cpp" "CMakeFiles/xhpcg.dir/src/CheckProblem.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/CheckProblem.cpp.o.d"
+  "/opt/hpcg/src/ComputeDotProduct.cpp" "CMakeFiles/xhpcg.dir/src/ComputeDotProduct.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeDotProduct.cpp.o.d"
+  "/opt/hpcg/src/ComputeDotProduct_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeDotProduct_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeDotProduct_ref.cpp.o.d"
+  "/opt/hpcg/src/ComputeMG.cpp" "CMakeFiles/xhpcg.dir/src/ComputeMG.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeMG.cpp.o.d"
+  "/opt/hpcg/src/ComputeMG_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeMG_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeMG_ref.cpp.o.d"
+  "/opt/hpcg/src/ComputeOptimalShapeXYZ.cpp" "CMakeFiles/xhpcg.dir/src/ComputeOptimalShapeXYZ.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeOptimalShapeXYZ.cpp.o.d"
+  "/opt/hpcg/src/ComputeProlongation_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeProlongation_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeProlongation_ref.cpp.o.d"
+  "/opt/hpcg/src/ComputeResidual.cpp" "CMakeFiles/xhpcg.dir/src/ComputeResidual.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeResidual.cpp.o.d"
+  "/opt/hpcg/src/ComputeRestriction_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeRestriction_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeRestriction_ref.cpp.o.d"
+  "/opt/hpcg/src/ComputeSPMV.cpp" "CMakeFiles/xhpcg.dir/src/ComputeSPMV.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeSPMV.cpp.o.d"
+  "/opt/hpcg/src/ComputeSPMV_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeSPMV_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeSPMV_ref.cpp.o.d"
+  "/opt/hpcg/src/ComputeSYMGS.cpp" "CMakeFiles/xhpcg.dir/src/ComputeSYMGS.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeSYMGS.cpp.o.d"
+  "/opt/hpcg/src/ComputeSYMGS_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeSYMGS_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeSYMGS_ref.cpp.o.d"
+  "/opt/hpcg/src/ComputeWAXPBY.cpp" "CMakeFiles/xhpcg.dir/src/ComputeWAXPBY.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeWAXPBY.cpp.o.d"
+  "/opt/hpcg/src/ComputeWAXPBY_ref.cpp" "CMakeFiles/xhpcg.dir/src/ComputeWAXPBY_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ComputeWAXPBY_ref.cpp.o.d"
+  "/opt/hpcg/src/ExchangeHalo.cpp" "CMakeFiles/xhpcg.dir/src/ExchangeHalo.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ExchangeHalo.cpp.o.d"
+  "/opt/hpcg/src/GenerateCoarseProblem.cpp" "CMakeFiles/xhpcg.dir/src/GenerateCoarseProblem.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/GenerateCoarseProblem.cpp.o.d"
+  "/opt/hpcg/src/GenerateGeometry.cpp" "CMakeFiles/xhpcg.dir/src/GenerateGeometry.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/GenerateGeometry.cpp.o.d"
+  "/opt/hpcg/src/GenerateProblem.cpp" "CMakeFiles/xhpcg.dir/src/GenerateProblem.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/GenerateProblem.cpp.o.d"
+  "/opt/hpcg/src/GenerateProblem_ref.cpp" "CMakeFiles/xhpcg.dir/src/GenerateProblem_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/GenerateProblem_ref.cpp.o.d"
+  "/opt/hpcg/src/MixedBaseCounter.cpp" "CMakeFiles/xhpcg.dir/src/MixedBaseCounter.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/MixedBaseCounter.cpp.o.d"
+  "/opt/hpcg/src/OptimizeProblem.cpp" "CMakeFiles/xhpcg.dir/src/OptimizeProblem.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/OptimizeProblem.cpp.o.d"
+  "/opt/hpcg/src/OutputFile.cpp" "CMakeFiles/xhpcg.dir/src/OutputFile.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/OutputFile.cpp.o.d"
+  "/opt/hpcg/src/ReadHpcgDat.cpp" "CMakeFiles/xhpcg.dir/src/ReadHpcgDat.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ReadHpcgDat.cpp.o.d"
+  "/opt/hpcg/src/ReportResults.cpp" "CMakeFiles/xhpcg.dir/src/ReportResults.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/ReportResults.cpp.o.d"
+  "/opt/hpcg/src/SetupHalo.cpp" "CMakeFiles/xhpcg.dir/src/SetupHalo.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/SetupHalo.cpp.o.d"
+  "/opt/hpcg/src/SetupHalo_ref.cpp" "CMakeFiles/xhpcg.dir/src/SetupHalo_ref.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/SetupHalo_ref.cpp.o.d"
+  "/opt/hpcg/src/TestCG.cpp" "CMakeFiles/xhpcg.dir/src/TestCG.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/TestCG.cpp.o.d"
+  "/opt/hpcg/src/TestNorms.cpp" "CMakeFiles/xhpcg.dir/src/TestNorms.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/TestNorms.cpp.o.d"
+  "/opt/hpcg/src/TestSymmetry.cpp" "CMakeFiles/xhpcg.dir/src/TestSymmetry.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/TestSymmetry.cpp.o.d"
+  "/opt/hpcg/src/WriteProblem.cpp" "CMakeFiles/xhpcg.dir/src/WriteProblem.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/WriteProblem.cpp.o.d"
+  "/opt/hpcg/src/YAML_Doc.cpp" "CMakeFiles/xhpcg.dir/src/YAML_Doc.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/YAML_Doc.cpp.o.d"
+  "/opt/hpcg/src/YAML_Element.cpp" "CMakeFiles/xhpcg.dir/src/YAML_Element.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/YAML_Element.cpp.o.d"
+  "/opt/hpcg/src/finalize.cpp" "CMakeFiles/xhpcg.dir/src/finalize.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/finalize.cpp.o.d"
+  "/opt/hpcg/src/init.cpp" "CMakeFiles/xhpcg.dir/src/init.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/init.cpp.o.d"
+  "/opt/hpcg/src/main.cpp" "CMakeFiles/xhpcg.dir/src/main.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/main.cpp.o.d"
+  "/opt/hpcg/src/mytimer.cpp" "CMakeFiles/xhpcg.dir/src/mytimer.cpp.o" "gcc" "CMakeFiles/xhpcg.dir/src/mytimer.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
